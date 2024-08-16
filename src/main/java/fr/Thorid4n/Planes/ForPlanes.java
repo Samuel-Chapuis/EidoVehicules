5d45c27029de0b95875a07d94c871f79cf499ddc
Samuel-Chapuis <samuel.chapuis@hotmail.com>
2024-08-16 12:29:13 +0200

    Adding simple items as a demo

--- src/main/java/fr/Thorid4n/Planes/ForPlanes.java
+++ src/main/java/fr/Thorid4n/Planes/ForPlanes.java
@@ -1,6 +1,10 @@
-package fr.Thorid4n.PlaneMod;
+package fr.Thorid4n.Planes;
 
 import com.mojang.logging.LogUtils;
+
+import fr.Thorid4n.Planes.item.ModCreativeModTabs;
+import fr.Thorid4n.Planes.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -14,13 +18,19 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(PlaneMode.MOD_ID)
-public class PlaneMode {
+@Mod(ForPlanes.MOD_ID)
+public class ForPlanes {
     public static final String MOD_ID = "forplanes";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public PlaneMode() {
+    public ForPlanes() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		ModCreativeModTabs.register(modEventBus);
+
+		ModItems.register(modEventBus);
+
+
 
         modEventBus.addListener(this::commonSetup);
 
@@ -34,7 +44,10 @@ public class PlaneMode {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+		if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+			event.accept(ModItems.SAPPHIRE);
+			event.accept(ModItems.RAW_SAPPHIRE);
+		}
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
